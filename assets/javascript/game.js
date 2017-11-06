f1bc16839d43b56267a6282ec9717305537252ea
xezian <xezian@icloud.com>
2017-11-06 12:08:45 -0700

    javascript program working needs a few more details to be added but working though

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,41 @@
 // declare variables (remaining guesses, wrong guesses, hidden word, words[], wins, losses)
 var user = {
-    guesses: 0,
+    guesses: 11,
     wrongGuesses: 0,
     wins: 0,
-    losses: "none"
+    losses: 0
 }
-var secretWords = ["pumpkin", "chicken", "porcupine", "pigeon", "squirrel", "weasel"];
-
-    // array into which to push first "_" to represent each letter and then each actual letter of the hidden word.
-var hiddenWord = "string";
+var secretWords = ["saguaro", "ocatillo", "cholla", "creosote", "prickly-pear", "mesquite", "palo-verde", "mormon-tea", "acacia", "brittlebush", "fishhook-barrel-cactus"];
+// randomly selected secret word becomes hiddenWord
+var hiddenWord = "";
+// array into which to push each actual letter of the hidden word
 var hiddenWordMap = [];
-
+// array into which to push "_" to represent each letter and then each actual letter of the hidden word.
+var displayWordMap =[];
+// function that resets the score at start of game
 var startingScore = function() {
     user.guesses = 11;
     user.wrongGuesses = 0;
     user.wins = 0;
     user.losses = 0;
 }
-
 // select at random one of the words from the secretWords array and set hiddenWord to that string
 // then push each letter into the array!
 var newWord = function() {
     hiddenWord = secretWords[Math.floor(Math.random() * secretWords.length)];
     hiddenWordMap = [];
+    displayWordMap = [];
     for (i = 0; i < hiddenWord.length; i++) {
         hiddenWordMap.push(hiddenWord.charAt(i));
+// push "-" for between two-word hiddenWords, and then "_" for every letter of hiddenWord
+        if (hiddenWord.charAt(i) === "-") {
+            displayWordMap.push(hiddenWord.charAt(i));
+        } else {
+        displayWordMap.push("_");
+        }
     }
-}
+};
+// testing stats to the console
 newWord();
 console.log(hiddenWordMap);
 newWord();
@@ -34,49 +43,64 @@ console.log(hiddenWordMap);
 console.log(user);
 startingScore();
 console.log(user);
-
-// display one "_" the hiddenWordMap array to represent each letter of the new hiddenWord
-//CODE >>>
-var inArray = function(needle,haystack) {
-    var count = haystack.length;
+// function that searches an array for 
+var inArray = function(x,y) {
+    var count = x.length;
     for ( var i = 0; i < count; i++ ) {
-        if ( haystack[i] === needle) {
+        if ( x[i] === y) {
             return true;
         }
     }
     return false;
 }
+var confirmNewWord = function() {
+    var ask = confirm("new word?");
+    if (ask) {
+        newWord();
+    } else {
+        var reset = confirm("start over?")
+        if (reset) {
+            startingState();
+        } 
+    }
+};
 
 var runGame = function() {
     document.onkeyup = function(event) {
     var keyPressed = event.key;
 // press a key to select a letter
-    if (inArray(keyPressed, hiddenWordMap)) {
+    if (inArray(hiddenWordMap, keyPressed)) {
 // get key presses and store the letters in their appropriate variables
     for (var i = 0; i < hiddenWordMap.length; i++) {
             if (keyPressed === hiddenWordMap[i]) {
                 alert(keyPressed + " is part of " + hiddenWord + "!") 
-                // decrease guesses by 1          
-                // reveal hidden letters when correctly guessed
-// CODE >>> 
+// decrease guesses by 1          
+                user.guesses--;
+// reveal hidden letters when correctly guessed
+// CODE >>>     
                 } 
             }
         } else {
             alert(keyPressed + " is not part of " + hiddenWord);
-            // decrease guesses by 1 and increases wrong guesses by 1
-    // CODE >>>
+// decrease guesses by 1 and increases wrong guesses by 1
+            user.guesses--;
+            user.wrongGuesses++;
         }
-    // when whole word is correctly guessed, increase wins(score) by 1
-        if (hiddenWordMap.toString === hiddenWord) {
-
-        }
+    };
+// when whole word is correctly guessed, increase wins(score) by 1
+    if (displayWordMap.toString === hiddenWord) {
+    alert("good job!")
+    user.wins++;
+    confirmNewWord();
     }
-};
-// get the key that is pressed
-
 // when remaining guesses run out, increase losses by 1
-
-// start game over with a new word
+    if (user.guesses < 1) {
+    alert("sorry you lose");
+    confirmNewWord();
+    }
+    // ask user if they want another word
+};
+// start game with a new word and new score
 var startingState = function() {
     document.onkeyup = function(event) {
     var keyPressed = event.key;
@@ -85,7 +109,7 @@ var startingState = function() {
         startingScore();
         newWord();
         runGame();
-        console.log(hiddenWordMap);
+        console.log(hiddenWordMap, displayWordMap);
         }
     }
 };
